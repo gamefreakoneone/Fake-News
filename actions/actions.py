380623e2ccb56332b5e3b42d4908dd5f5354ce4e
gamefreakoneone <amogh@outlook.com>
2021-06-27 13:08:36 +0530

    Upload

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,6 @@
+import requests
+import bs4
+
 # This files contains your custom actions which can be used to run
 # custom Python code.
 #
@@ -7,10 +10,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +28,19 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class Fact_Checker(Action):
+    def name(self) -> Text:
+        return "action_fact_check"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        text= tracker.get_slot("Event_check")
+        url = 'https://google.com/search?q=' + text
+        request_result = requests.get( url )
+        soup = bs4.BeautifulSoup( request_result.text 
+                         , "html.parser" )
+        temp = soup.find( "div" , class_='BNeawe' ).text 
+    
+        print( temp )
